156363d5be74ed0abd7112806b3f4cfe1d0ff156
billiemead <bmead@mjhlifesciences.com>
2023-11-13 20:12:16 -0500

    move scripts to functions.php

--- wp-content/themes/elainestudy/functions.php
+++ wp-content/themes/elainestudy/functions.php
@@ -24,40 +24,97 @@ function es_assets()
 }
 add_action('wp_enqueue_scripts', 'es_assets');
 
+// Add HubSpot script to page head
+function hubspot_javascript() {
+    ?>
+        <!-- Start of HubSpot Embed Code -->
+        <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/21985033.js"></script>
+        <!-- End of HubSpot Embed Code -->
+    <?php
+}
+add_action('wp_head', 'hubspot_javascript');
+
+// Add Google Analytics script to page head
+function ganalytics_javascript()
+{
+?>
+    <!-- Global site tag - Google Analytics Start -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-157942511-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-157942511-1');
+    </script>
+    <!-- Global site tag - Google Analytics End -->
+<?php
+}
+add_action('wp_head', 'ganalytics_javascript');
+
+// Add HotJar script to page head
+function hotjar_javascript()
+{
+?>
+    <!-- Hotjar Tracking Code for https://discoverelaine.com/ -->
+    <script>
+        (function(h, o, t, j, a, r) {
+            h.hj = h.hj || function() {
+                (h.hj.q = h.hj.q || []).push(arguments)
+            };
+            h._hjSettings = {
+                hjid: 2799615,
+                hjsv: 6
+            };
+            a = o.getElementsByTagName('head')[0];
+            r = o.createElement('script');
+            r.async = 1;
+            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+    </script>
+    <!-- Hotjar Tracking Code for discoverelaine.com End -->
+    <?php
+}
+add_action('wp_head', 'hotjar_javascript');
+
 function marker_io()
 {
     $current_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ?
         "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
     if ($current_url == 'https://dev-elaine-study.pantheonsite.io/' || 'https://elaine-study.localdev') {
-        ?>
-                <script>
-                    window.markerConfig = {
-                        project: '65526afda84403da6009b11d',
-                        source: 'snippet'
-                    };
+    ?>
+        <script>
+            window.markerConfig = {
+                project: '65526afda84403da6009b11d',
+                source: 'snippet'
+            };
 
-                    ! function(e, r, a) {
-                        if (!e.__Marker) {
-                            e.__Marker = {};
-                            var t = [],
-                                n = {
-                                    __cs: t
-                                };
-                            ["show", "hide", "isVisible", "capture", "cancelCapture", "unload", "reload", "isExtensionInstalled", "setReporter", "setCustomData", "on", "off"].forEach(function(e) {
-                                n[e] = function() {
-                                    var r = Array.prototype.slice.call(arguments);
-                                    r.unshift(e), t.push(r)
-                                }
-                            }), e.Marker = n;
-                            var s = r.createElement("script");
-                            s.async = 1, s.src = "https://edge.marker.io/latest/shim.js";
-                            var i = r.getElementsByTagName("script")[0];
-                            i.parentNode.insertBefore(s, i)
+            ! function(e, r, a) {
+                if (!e.__Marker) {
+                    e.__Marker = {};
+                    var t = [],
+                        n = {
+                            __cs: t
+                        };
+                    ["show", "hide", "isVisible", "capture", "cancelCapture", "unload", "reload", "isExtensionInstalled", "setReporter", "setCustomData", "on", "off"].forEach(function(e) {
+                        n[e] = function() {
+                            var r = Array.prototype.slice.call(arguments);
+                            r.unshift(e), t.push(r)
                         }
-                    }(window, document);
-                </script>
-        <?php
+                    }), e.Marker = n;
+                    var s = r.createElement("script");
+                    s.async = 1, s.src = "https://edge.marker.io/latest/shim.js";
+                    var i = r.getElementsByTagName("script")[0];
+                    i.parentNode.insertBefore(s, i)
+                }
+            }(window, document);
+        </script>
+<?php
     }
 }
 add_action('wp_head', 'marker_io');
