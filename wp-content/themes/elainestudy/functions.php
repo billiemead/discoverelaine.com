96563782de815549e525c6de18b6d0149c5d3c7b
billiemead <bmead@mjhlifesciences.com>
2023-11-19 14:34:13 -0500

    Now recruiting fixes

--- wp-content/themes/elainestudy/functions.php
+++ wp-content/themes/elainestudy/functions.php
@@ -90,29 +90,11 @@ function marker_io()
     ?>
         <script>
             window.markerConfig = {
-                project: '65526afda84403da6009b11d',
-                source: 'snippet'
-            };
+            project: '6557e8cf503bc7b5ebac2a0b',
+            source: 'snippet'
+        };
 
-            ! function(e, r, a) {
-                if (!e.__Marker) {
-                    e.__Marker = {};
-                    var t = [],
-                        n = {
-                            __cs: t
-                        };
-                    ["show", "hide", "isVisible", "capture", "cancelCapture", "unload", "reload", "isExtensionInstalled", "setReporter", "setCustomData", "on", "off"].forEach(function(e) {
-                        n[e] = function() {
-                            var r = Array.prototype.slice.call(arguments);
-                            r.unshift(e), t.push(r)
-                        }
-                    }), e.Marker = n;
-                    var s = r.createElement("script");
-                    s.async = 1, s.src = "https://edge.marker.io/latest/shim.js";
-                    var i = r.getElementsByTagName("script")[0];
-                    i.parentNode.insertBefore(s, i)
-                }
-            }(window, document);
+            !function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};["show","hide","isVisible","capture","cancelCapture","unload","reload","isExtensionInstalled","setReporter","setCustomData","on","off"].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement("script");s.async=1,s.src="https://edge.marker.io/latest/shim.js";var i=r.getElementsByTagName("script")[0];i.parentNode.insertBefore(s,i)}}(window,document);
         </script>
 <?php
     }
