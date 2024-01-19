9d26fbb327ce11cb5d70b488104f4ac355f721a9
Billie Mead <bmead@mjhlifesciences.com>
2024-01-19 14:54:00 -0500

    remove marker.io cvode from functions.php

--- wp-content/themes/elainestudy/functions.php
+++ wp-content/themes/elainestudy/functions.php
@@ -99,22 +99,3 @@ function custom_class($classes)
     }
     return $classes;
 }
-
-function marker_io() {
-    $current_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ?
-        "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-
-    if ($current_url == 'https://dev-elaine-study.pantheonsite.io/' || 'https://elaine-study.localdev') {
-    ?>
-        <script>
-            window.markerConfig = {
-            project: '6557e8cf503bc7b5ebac2a0b',
-            source: 'snippet'
-        };
-
-            !function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};["show","hide","isVisible","capture","cancelCapture","unload","reload","isExtensionInstalled","setReporter","setCustomData","on","off"].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement("script");s.async=1,s.src="https://edge.marker.io/latest/shim.js";var i=r.getElementsByTagName("script")[0];i.parentNode.insertBefore(s,i)}}(window,document);
-        </script>
-    <?php
-    }
-}
-add_action('wp_head', 'marker_io');
